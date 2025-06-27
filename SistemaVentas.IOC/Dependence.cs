2b07937013250edf105a730559d28ba3e599ab0d
DanielM <danielmarinm0510@gmail.com>
2025-06-27 18:20:30 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVentas.IOC/Dependence.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVentas.DAL.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.IOC
+{
+    public static class Dependence
+    {
+        public static void InjectDependencies(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+    }
+}
